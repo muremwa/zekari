f6c104e2bda6155feeea7bcc407f9efdffa15b23
Daniel Muremwa Mburu <muremwa@users.noreply.github.com>
2024-01-31 10:19:28 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { authGuard } from "./guards/auth/auth.guard";
 import { StudentsDashboardComponent } from "./components/students/students-dashboard/students-dashboard.component";
+import { TeacherDashboardComponent } from "./components/teachers/teacher-dashboard/teacher-dashboard.component";
+import { AdminDashboardComponent } from "./components/admins/admin-dashboard/admin-dashboard.component";
 
 export const routes: Routes = [
     { path: "sign-in", component: LoginComponent },
     { path: "students", component: StudentsDashboardComponent, canActivate: [authGuard] },
+    { path: "teachers", component: TeacherDashboardComponent, canActivate: [authGuard] },
+    { path: "admin", component: AdminDashboardComponent, canActivate: [authGuard] }
 ];
